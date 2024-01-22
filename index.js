f65b5cfef70500e9d6a7ec5c74335cfe3d6255e6
David <Marin9737@gmail.com>
2024-01-22 02:40:58 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+module.exports = { cats, destructivelyAppendCat };
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+module.exports = { cats, destructivelyPrependCat };
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+module.exports = { cats, destructivelyRemoveLastCat };
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+module.exports = { cats, destructivelyRemoveLastCat };
+
+function appendCat(name) {
+  const newCatsArray = cats.concat(name);
+  return newCatsArray;
+}
+
+module.exports = { cats, appendCat };
+
+function prependCat(name) {
+  const newCatsArray = [name].concat(cats);
+  return newCatsArray;
+}
+
+module.exports = { cats, prependCat };
+
+function removeLastCat() {
+  const newCatsArray = cats.slice(0, -1);
+  return newCatsArray;
+}
+module.exports = { cats, removeLastCat };
+
+function removeFirstCat() {
+  const newCatsArray = cats.slice(1);
+  return newCatsArray;
+}
+
+module.exports = { cats, removeFirstCat };
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+module.exports = { cats, destructivelyRemoveFirstCat };
